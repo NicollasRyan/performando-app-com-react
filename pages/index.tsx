170294d2cb6e3936697a883ec26e4e49841814c1
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-11-30 12:25:51 -0300

    useCallback

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,10 @@ export default function Home() {
     setResults(data);
   }
 
+  async function AddToWishList(id: number) {
+    console.log(id);
+  }
+
   return (
     <div>
       <h1>Search</h1>
@@ -31,7 +35,7 @@ export default function Home() {
         <button type="submit">Buscar</button>
       </form>
 
-      <SearchResults results={results} />
+      <SearchResults results={results} onAddToWishList={AddToWishList} />
     </div>
   );
 }
